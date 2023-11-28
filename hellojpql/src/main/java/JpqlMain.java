3c4317515a9c03c795ffaab7d2b5595963c35b16
leejaehun <ljh0901@mlsoft.com>
2023-11-28 14:55:26 +0900

    Named 쿼리

--- hellojpql/src/main/java/JpqlMain.java
+++ hellojpql/src/main/java/JpqlMain.java
@@ -1,4 +1,5 @@
 import jpql.Member;
+import jpql.Team;
 
 import javax.persistence.*;
 import java.util.List;
@@ -13,12 +14,40 @@ public class JpqlMain {
 
         try {
 
-            Member member = new Member();
-            member.setUsername("member1");
-            em.persist(member);
+            Team team1 = new Team();
+            team1.setName("팀A");
+            Team team2 = new Team();
+            team2.setName("팀B");
+            Team team3 = new Team();
+            team3.setName("팀C");
 
-            List<Member> resultList = em.createQuery("select m from Member m", Member.class)
+            em.persist(team1);
+            em.persist(team2);
+            em.persist(team3);
+
+            Member member1 = new Member();
+            member1.setUsername("member1");
+            member1.setTeam(team1);
+
+            Member member2 = new Member();
+            member2.setUsername("member2");
+            member2.setTeam(team2);
+
+            Member member3 = new Member();
+            member3.setUsername("member3");
+            member3.setTeam(team2);
+
+            em.persist(member1);
+            em.persist(member2);
+            em.persist(member3);
+
+            em.flush();
+            em.clear();
+
+            List<Member> resultList = em.createNamedQuery("Member.findByUsername", Member.class)
+                    .setParameter("username", "member1")
                     .getResultList();
+            System.out.println(resultList.size());
 
             tx.commit();
         } catch (Exception e) {
--- hellojpql/src/main/java/jpql/Member.java
+++ hellojpql/src/main/java/jpql/Member.java
@@ -3,6 +3,10 @@ package jpql;
 import javax.persistence.*;
 
 @Entity
+@NamedQuery(
+        name = "Member.findByUsername",
+        query = "select m from Member m where m.username = :username"
+)
 public class Member {
     @Id @GeneratedValue
     private Long id;
@@ -12,6 +16,14 @@ public class Member {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "TEAM_ID")
     private Team team;
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
 
     public Long getId() {
         return id;
@@ -35,5 +47,15 @@ public class Member {
 
     public void setAge(int age) {
         this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", age=" + age +
+                ", team=" + team +
+                '}';
     }
 }
