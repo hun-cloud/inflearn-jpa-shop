36e2766e1f47188374f82bfb53bd65c69f66b750
leejaehun <ljh0901@mlsoft.com>
2023-11-28 15:10:31 +0900

    강의 종료

--- hellojpql/src/main/java/JpqlMain.java
+++ hellojpql/src/main/java/JpqlMain.java
@@ -13,29 +13,14 @@ public class JpqlMain {
         tx.begin();
 
         try {
-
-            Team team1 = new Team();
-            team1.setName("팀A");
-            Team team2 = new Team();
-            team2.setName("팀B");
-            Team team3 = new Team();
-            team3.setName("팀C");
-
-            em.persist(team1);
-            em.persist(team2);
-            em.persist(team3);
-
             Member member1 = new Member();
             member1.setUsername("member1");
-            member1.setTeam(team1);
 
             Member member2 = new Member();
             member2.setUsername("member2");
-            member2.setTeam(team2);
 
             Member member3 = new Member();
             member3.setUsername("member3");
-            member3.setTeam(team2);
 
             em.persist(member1);
             em.persist(member2);
@@ -44,10 +29,16 @@ public class JpqlMain {
             em.flush();
             em.clear();
 
-            List<Member> resultList = em.createNamedQuery("Member.findByUsername", Member.class)
-                    .setParameter("username", "member1")
-                    .getResultList();
-            System.out.println(resultList.size());
+            int count = em.createQuery("update Member m set m.age = 20")
+                    .executeUpdate();
+            em.flush();
+            em.clear();
+
+            Member findMember = em.find(Member.class, member1.getId());
+            System.out.println(findMember);
+
+            System.out.println(member1.getAge());
+            System.out.println(count);
 
             tx.commit();
         } catch (Exception e) {
